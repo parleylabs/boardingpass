823b9901b6ab6d71ec49851242614ce536595c88
Jonathan Bao <jonathanbao@jonathans-mbp.lan>
2023-05-01 19:07:37 -0700

    updated login url to admin login

--- backend/api/models/user.py
+++ backend/api/models/user.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, UserManager, BaseUserManager
 from api.models.organization import Organization
+from django.contrib.auth.models import PermissionsMixin
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     user_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=75, unique=True)
     first_name = models.CharField(max_length=75)
